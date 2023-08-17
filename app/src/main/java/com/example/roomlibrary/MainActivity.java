3d7ea111c35b4a7606f94144117b945f82743e74
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-17 17:55:05 +0600

    save data with room

--- app/src/main/java/com/example/roomlibrary/MainActivity.java
+++ app/src/main/java/com/example/roomlibrary/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.roomlibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button saveBtn;
+    ListView listView;
     EditText editName, edtAmount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
         edtAmount = findViewById(R.id.edtAmount);
 
 
+        DatabaseHelper databaseHelper = DatabaseHelper.getDB(this);
 
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String title = editName.getText().toString();
+                String ammount = edtAmount.getText().toString();
+
+                databaseHelper.dataAccessObject().addTx(
+                        new ExpenseEntity(title,ammount)
+                );
+
+                Toast.makeText(MainActivity.this, "Data Save", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+//        databaseHelper.dataAccessObject().deleteTx();
+//        databaseHelper.dataAccessObject().updateTx();
     }
 }
